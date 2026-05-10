38386183d1396f5e754b6847704f5258a49b4050
Pablo Ricalde Soares <ricaldesoares@gmail.com>
2026-05-10 14:10:43 -0300

    Update teste_sensor.ino

--- Testes/teste_sensor/teste_sensor.ino
+++ Testes/teste_sensor/teste_sensor.ino
@@ -1,37 +1,47 @@
-#include <WiFi.h>
+#include <HardwareSerial.h>
 
-// Nome e senha da rede que o ESP32 vai criar
-const char *ssid = "ESP32_Cronometragem";
-const char *password = "teste123456"; // A senha precisa ter no mínimo 8 caracteres
+// Inicializar porta Serial 2 do ESP
+HardwareSerial tfLunaSerial(2); 
 
 void setup() {
+  // Monitor Serial
   Serial.begin(115200);
-  Serial.println();
-
-  Serial.println("Configurando o Access Point...");
   
-  // Inicia o Wi-Fi em modo Access Point (AP)
-  WiFi.softAP(ssid, password);
-
-  // Pega o endereço de IP do ESP32 (por padrão costuma ser 192.168.4.1)
-  IPAddress IP = WiFi.softAPIP();
+  // Serial do TF-Luna (Baud rate padrão é 115200, pinos RX=16, TX=17)
+  tfLunaSerial.begin(115200, SERIAL_8N1, 16, 17); 
   
-  Serial.print("Rede Wi-Fi criada com sucesso! Nome: ");
-  Serial.println(ssid);
-  Serial.print("Endereço IP do ESP32: ");
-  Serial.println(IP);
+  Serial.println("Iniciando leitura do TF-Luna...");
 }
 
 void loop() {
-  // O ESP32 indica quantas pessoas/celulares estão conectados na rede dele
-  static int usuarios_anteriores = 0;
-  int usuarios_conectados = WiFi.softAPgetStationNum();
+  // O formato padrão do TF-Luna envia frames de 9 bytes
+  if (tfLunaSerial.available() >= 9) {
+    
+    // O pacote de dados sempre começa com dois bytes de cabeçalho: 0x59 e 0x59
+    if (tfLunaSerial.read() == 0x59) {
+      if (tfLunaSerial.read() == 0x59) {
+        
+        // Lê os próximos bytes (Distância, Força do Sinal, Temperatura e Checksum)
+        int dist_L = tfLunaSerial.read();
+        int dist_H = tfLunaSerial.read();
+        int amp_L = tfLunaSerial.read();
+        int amp_H = tfLunaSerial.read();
+        int temp_L = tfLunaSerial.read();
+        int temp_H = tfLunaSerial.read();
+        int checksum = tfLunaSerial.read(); // Lemos para esvaziar o buffer, mesmo sem validar (para o sistema real validaremos)
 
-  if (usuarios_conectados != usuarios_anteriores) {
-    Serial.print("Dispositivos conectados agora: ");
-    Serial.println(usuarios_conectados);
-    usuarios_anteriores = usuarios_conectados;
+        // A distância real é a combinação do byte Alto (High) e Baixo (Low)
+        int distance = dist_L + (dist_H << 8);
+        
+        // A força do sinal (Amplitude) também usa dois bytes
+        int strength = amp_L + (amp_H << 8);
+
+        // Imprime os resultados no computador
+        Serial.print("Distância: ");
+        Serial.print(distance);
+        Serial.print(" cm , Força do Sinal: ");
+        Serial.println(strength);
+      }
+    }
   }
-  
-  delay(1000);
 }
